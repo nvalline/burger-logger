325a8da7ea1024b471c3b66b52ae246e06761717
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-05-20 15:10:03 -0600

    created ul on index to display data

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,11 +5,8 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     burger.selectAll(data => {
-
-        console.log(data)
-
         const hbsObject = {
-            burger: data
+            burgers: data
         };
 
         console.log(`hbsObject: ${hbsObject}`)
