2b300b21620ccf53f20913e865f7cd094251b783
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-05-20 13:51:23 -0600

    created connection.js

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+});
+
+connection.connect(err => {
+    if (err) {
+        console.log(`Error connecting: ${err.stack}`)
+        return;
+    };
+
+    console.log(`Connected by id ${connection.threadId}`)
+});
+
+module.exports = connection;
