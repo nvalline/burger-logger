584bf6e48085dc0a909e2be7e2f3edcd1144df4c
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-05-26 21:28:41 -0600

    updated db files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
 -- Seed data for burgers table
-INSERT INTO burgers (burger_name) VALUES ("Regular Hamburger"), ("Double Bacon Cheeseburger"), ("Jalapeno Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Regular Hamburger"), ("Double Bacon Cheeseburger"), ("Jalapeno Bacon Burger"), ("Peanut Butter Burger");
+
+UPDATE burgers SET devoured=true WHERE id=2;
+UPDATE burgers SET devoured=true WHERE id=4;
