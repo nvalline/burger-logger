315a392e418e5189b8293e0da1195fd5b21bd5f9
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-05-27 22:22:55 -0600

    adjusted schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,5 +9,6 @@ CREATE TABLE burgers (
     id INT AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
